76d21039f2fbc41ff9d78feee2e1e305fcade3a7
Marcu Sabin <sabinmarcu@gmail.com>
2013-10-10 23:26:38 +0100

    [patch] Added ECS Proxies

--- Challenge1/main.java
+++ Challenge1/main.java
@@ -1,1 +1,12 @@
-public class main{ public static void main(String[] args){ (new Runner()).run(); } }
+public class main{ 
+	public static void main(String[] args){ 
+
+		// Just in case ...
+		System.getProperties().put("proxySet", "true");
+		System.getProperties().put("proxyHost", "152.78.128.51");
+		System.getProperties().put("proxyPort", "3128");
+
+		// Start the app ...
+		(new Runner()).run(); 
+	} 
+}
